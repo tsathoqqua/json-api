bc231415c8a18badcb2e4793d62010aab8d3c0d2
Tsathoqqua <tsathoqqua@gmail.com>
2023-12-12 17:59:17 +0100

    Remove old index

--- fakeapis/index.js
+++
@@ -1,9 +0,0 @@
-const users = require('./users.json');
-const posts = require('./posts.json');
-// Something more
-
-module.exports = () => ({
-  users: users,
-  posts: posts
-  // Something more
-});
